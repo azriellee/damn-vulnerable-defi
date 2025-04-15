47fab63ab6e36d83c0e98df917cc308dfa5a5b87
azriellee <e0725561@u.nus.edu>
2025-04-15 16:26:11 +0800

    WIP: before installing forge-std

--- test/unstoppable/Unstoppable.t.sol
+++ test/unstoppable/Unstoppable.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Damn Vulnerable DeFi v4 (https://damnvulnerabledefi.xyz)
-pragma solidity =0.8.25;
+pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
